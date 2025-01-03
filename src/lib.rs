b445d9fafcc63299ff15163c10de4bb703ea2cb2
jannikac <jannikac@mailbox.org>
2025-01-03 21:18:56 +0100

    update: properly handle ipv6 dual stack

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
+use local_ip_address::{local_ip, local_ipv6};
 use log::{debug, info};
 use serde::{Deserialize, Serialize};
 use services::dyndns2::Dyndns2;
-use std::{collections::HashMap, env, fs, net::IpAddr, path::PathBuf, str::FromStr};
+use std::{collections::HashMap, env, fmt::Display, fs, net::IpAddr, path::PathBuf};
 use url::Url;
 mod services;
 
@@ -12,6 +13,23 @@ pub struct Config {
     pub interval: u64,
     pub ip_webservice: Url,
     pub services: Vec<ServiceTypes>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
+pub struct WanIps {
+    ip: Option<IpAddr>,
+    ipv6: Option<IpAddr>,
+}
+
+impl Display for WanIps {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "IPV4: {}, IPV6: {}",
+            self.ip.map_or("None".to_string(), |v| v.to_string()),
+            self.ipv6.map_or("None".to_string(), |v| v.to_string())
+        )
+    }
 }
 
 impl Config {
@@ -53,26 +71,26 @@ pub trait ServiceTrait {
         format!("{}@{}", self.get_identifier(), self.get_server())
     }
     /// This function should update the remote service with the wan_ip adress parameter
-    async fn update_remote(&self, wan_ip: IpAddr) -> Result<()>;
+    async fn update_remote(&self, wan_ip: &WanIps) -> Result<()>;
     /** This function should check the cache and call update_remote and update the cache with the ip if
      * the ip address is not in the cache or do nothing if it is in the cache
      */
-    async fn update(&self, wan_ip: IpAddr, cache: &mut Cache) -> Result<()> {
+    async fn update(&self, wan_ip: WanIps, cache: &mut Cache) -> Result<()> {
         match cache.get(self.get_cache_key()) {
             Some(cache_ip) => {
-                // cache key found. if cache_ip is same as wan_ip dont do anythink
+                // cache key found. if cache_ip is same as wan_ip dont do anything
                 if cache_ip == &wan_ip {
                     info!(
                         "Cache IP ({}) and WAN IP ({}) are identical, not updating remote ip",
                         cache_ip, wan_ip
                     )
                 } else {
-                    self.update_remote(wan_ip).await?;
+                    self.update_remote(&wan_ip).await?;
                 }
             }
             None => {
                 //cache key not found, update it and add ip to cache
-                self.update_remote(wan_ip).await?;
+                self.update_remote(&wan_ip).await?;
                 cache.insert(self.get_cache_key(), wan_ip)?;
             }
         }
@@ -83,17 +101,17 @@ pub trait ServiceTrait {
 #[derive(Serialize, Deserialize)]
 pub struct Cache {
     pub path: PathBuf,
-    cache: HashMap<String, IpAddr>,
+    cache: HashMap<String, WanIps>,
 }
 
 impl Cache {
-    pub fn insert(&mut self, k: String, v: IpAddr) -> Result<()> {
+    pub fn insert(&mut self, k: String, v: WanIps) -> Result<()> {
         self.cache.insert(k, v);
         let bytes = bincode::serialize(self)?;
         fs::write(&self.path, bytes)?;
         Ok(())
     }
-    pub fn get(&self, k: String) -> Option<&IpAddr> {
+    pub fn get(&self, k: String) -> Option<&WanIps> {
         self.cache.get(&k)
     }
     pub fn new() -> Result<Cache> {
@@ -111,7 +129,7 @@ impl Cache {
                 "No cache file found in ({}), creating one",
                 cache_path.canonicalize()?.to_string_lossy()
             );
-            let cache: HashMap<String, IpAddr> = HashMap::new();
+            let cache: HashMap<String, WanIps> = HashMap::new();
             return Ok(Cache {
                 path: cache_path,
                 cache,
@@ -120,15 +138,43 @@ impl Cache {
     }
 }
 
-pub async fn get_wan_ip(ip_webservice_url: Url) -> Result<IpAddr> {
-    let ip_raw = reqwest::get(ip_webservice_url).await?.text().await?;
-    let ip = IpAddr::from_str(&ip_raw.trim())?;
-    Ok(ip)
+enum IpKind {
+    V4,
+    V6,
+}
+
+async fn get_ip(ip_webservice_url: Url, kind: IpKind) -> Result<IpAddr> {
+    let local_ip = match kind {
+        IpKind::V4 => local_ip()?,
+        IpKind::V6 => local_ipv6()?,
+    };
+    let client = reqwest::Client::builder()
+        .local_address(local_ip)
+        .build()
+        .unwrap();
+    let wan_ip_raw = client.get(ip_webservice_url).send().await?.text().await?;
+    Ok(wan_ip_raw.trim().parse()?)
+}
+
+pub async fn get_wan_ip(ip_webservice_url: Url) -> Result<WanIps> {
+    let ip = get_ip(ip_webservice_url.clone(), IpKind::V4)
+        .await
+        .map_err(|e| debug!("{}", e))
+        .ok();
+    let ipv6 = get_ip(ip_webservice_url.clone(), IpKind::V6)
+        .await
+        .map_err(|e| debug!("{}", e))
+        .ok();
+    if ip.is_none() && ipv6.is_none() {
+        return Err(anyhow!("No WAN ipv4 or ipv6 could be determined"));
+    }
+
+    Ok(WanIps { ip, ipv6 })
 }
 
 pub async fn handle_service(
     service: &ServiceTypes,
-    wan_ip: IpAddr,
+    wan_ip: WanIps,
     cache: &mut Cache,
 ) -> Result<()> {
     match service {
--- src/services/dyndns2.rs
+++ src/services/dyndns2.rs
@@ -1,12 +1,12 @@
-use std::net::IpAddr;
-
 use crate::ServiceTrait;
 
+use crate::WanIps;
 use anyhow::{Ok, Result};
 use async_trait::async_trait;
 use log::info;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Dyndns2 {
@@ -24,22 +24,20 @@ impl ServiceTrait for Dyndns2 {
     fn get_identifier(&self) -> &String {
         &self.identifier
     }
-    async fn update_remote(&self, wan_ip: IpAddr) -> Result<()> {
-        let update_url: String = match wan_ip {
-            IpAddr::V4(ip) => format!(
-                "https://{server}/nic/update?hostname={identifier}&myip={ip}",
-                server = self.server,
-                identifier = self.identifier
-            ),
-            IpAddr::V6(ip) => format!(
-                "https://{server}/nic/update?hostname={identifier}&myipv6={ip}",
-                server = self.server,
-                identifier = self.identifier
-            ),
-        };
+    async fn update_remote(&self, wan_ip: &WanIps) -> Result<()> {
+        let mut params = vec![];
+        if let Some(ip) = wan_ip.ip {
+            params.push(("myip", ip.to_string()));
+        }
+        if let Some(ip) = wan_ip.ipv6 {
+            params.push(("myipv6", ip.to_string()));
+        }
+        params.push(("hostname", self.server.clone()));
+
+        let url = Url::parse_with_params(&format!("https://{}/nic/update", self.server), &params)?;
         let client = Client::new();
         let res = client
-            .get(update_url)
+            .get(url)
             .basic_auth(&self.login, Some(&self.password))
             .send()
             .await?;
